898f0ec5dcb1ad8237439d16219a7ab9fc2c9117
dphard <345976408@qq.com>
2025-05-16 23:30:32 +0800

    add make shaders

---
+++ src/first_app.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "lve_pipeline.hpp"
+#include "lve_window.hpp"
+
+namespace lve {
+
+class FirstApp {
+public:
+  static constexpr int WIDTH = 800;
+  static constexpr int HEIGHT = 600;
+
+  void run();
+
+private:
+  // LveWindow LveWindow(WIDTH, HEIGHT, "hello vulkan");
+  LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+  LvePipeline pipeline{"shaders/simple_shader.vert.spv",
+                       "shaders/simple_shader.frag.spv"};
+};
+} // namespace lve
