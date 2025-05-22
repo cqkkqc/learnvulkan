95267949f412dae5286f1c9aad13639066aba8de
dphard <345976408@qq.com>
2025-05-22 22:02:38 +0800

    push constants

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -6,6 +6,10 @@
 #include <stdexcept>
 
 namespace lve {
+struct SimplePushConstantData {
+  glm::vec2 offset;
+  alignas(16) glm::vec3 color;
+};
 
 FirstApp::FirstApp() {
   loadModels();
@@ -35,12 +39,18 @@ void FirstApp::loadModels() {
 }
 
 void FirstApp::createPipelineLayout() {
+  VkPushConstantRange pushConstantRange{};
+  pushConstantRange.stageFlags =
+      VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+  pushConstantRange.offset = 0;
+  pushConstantRange.size = sizeof(SimplePushConstantData);
+
   VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
   pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
   pipelineLayoutInfo.setLayoutCount = 0;
   pipelineLayoutInfo.pSetLayouts = nullptr;
-  pipelineLayoutInfo.pushConstantRangeCount = 0;
-  pipelineLayoutInfo.pPushConstantRanges = nullptr;
+  pipelineLayoutInfo.pushConstantRangeCount = 1;
+  pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
   if (vkCreatePipelineLayout(lveDevice.device(), &pipelineLayoutInfo, nullptr,
                              &pipelineLayout) != VK_SUCCESS) {
     throw std::runtime_error("failed to create pipeline layout!");
@@ -106,6 +116,8 @@ void FirstApp::freeCommandBuffers() {
 }
 
 void FirstApp::recordCommandBuffer(int imageIndex) {
+  static int frame = 30;
+  frame = (frame + 1) % 100;
   VkCommandBufferBeginInfo beginInfo{};
   beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -147,6 +159,17 @@ void FirstApp::recordCommandBuffer(int imageIndex) {
   lveModel->bind(commandBuffers[imageIndex]);
   lveModel->draw(commandBuffers[imageIndex]);
 
+  for (int j = 0; j < 4; j++) {
+    SimplePushConstantData push{};
+    push.offset = {-0.5f + frame * 0.02f, -0.4f + j * 0.25f};
+    push.color = {0.0f, 0.0f, 0.2f + 0.2f * j};
+
+    vkCmdPushConstants(commandBuffers[imageIndex], pipelineLayout,
+                       VK_SHADER_STAGE_VERTEX_BIT |
+                           VK_SHADER_STAGE_FRAGMENT_BIT,
+                       0, sizeof(SimplePushConstantData), &push);
+    lveModel->draw(commandBuffers[imageIndex]);
+  }
   vkCmdEndRenderPass(commandBuffers[imageIndex]);
   if (vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS) {
     throw std::runtime_error("failed to record command buffer!");
